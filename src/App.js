bc8ce7e0f8a8aca2e4cffafd43a0e9317478da4b
melvin.rodriguez <mr.arteaga06@gmail.com>
2023-09-02 23:30:49 -0600

    commit 9-2-2023

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-
 import './App.css';
 import { Button } from '@mui/material';
-
-
 
 function App() {
   return (
     <div>
       
-
-      <Button  color="secondary">
-        COLOR
-      </Button>
+      <br></br>
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+          <img 
+            alt="Licencia de Creative Commons" 
+            style={{ borderWidth: 0 }} 
+            src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" 
+          />
+        </a>
     </div>
-
-
   );
 }
 
 export default App;
+
+
